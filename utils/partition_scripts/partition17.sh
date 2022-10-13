a3a35d74a60b576a78e536dbbdbe661705973b37
AntoninKns <antonin.kenens@etu.enseeiht.fr>
2022-10-13 16:08:27 -0400

    Splitting module

--- utils/partition_scripts/partition17.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-julia --project -E 'using Pkg; Pkg.instantiate(); Pkg.update(); Pkg.resolve()'
-julia --project benchmark/distributed_benchmarks.jl 17
--- utils/retrieve_logs_windows.sh
+++
@@ -1,2 +0,0 @@
-scp p114804@frontal22.recherche.polymtl.ca:/home/p114804/LevenbergMarquardt.jl/benchmark_files/log_error/* benchmark_files/log_error/
-scp p114804@frontal22.recherche.polymtl.ca:/home/p114804/LevenbergMarquardt.jl/benchmark_files/log_output/* benchmark_files/log_output/
